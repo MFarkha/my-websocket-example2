179d4626406181446abe1d3ba466a4466a93131e
Farhad Maten <farhad.maten@gmail.com>
2024-02-22 08:10:43 -0500

    added cluster support

--- src/server.js
+++ src/server.js
@@ -1,18 +1,33 @@
+const cluster = require('cluster');
+const { availableParallelism } = require('os');
+const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
+
 const http = require('http');
 const { Server } = require('socket.io');
 
 const api = require('./api');
 const sockets = require('./sockets');
 
-const PORT = 3000 || process.env.PORT;
+if (cluster.isPrimary) {
+    const numCPUs = availableParallelism();
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork({
+            PORT: 3000 + i
+        })
+    }
+    return setupPrimary();
+}
+// const PORT = 3000 || process.env.PORT;
 
 async function startServer() {
     const httpServer = http.createServer(api);
     const socketServer = new Server(httpServer, {
-        connectionStateRecovery: {}
+        connectionStateRecovery: {},
+        adapter: createAdapter(),
     });
-    httpServer.listen(PORT, () => {
-        console.log('Listening the port: ', PORT);
+    const port = process.env.PORT;
+    httpServer.listen(port, () => {
+        console.log('Listening the port: ', port);
     })
     sockets.listen(socketServer);
 }
